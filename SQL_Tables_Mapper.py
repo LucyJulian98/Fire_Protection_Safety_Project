0e0e150a6efee1cc9645f2cd982357a484baacd3
LucyJulian98 <llj40@georgetown.edu>
2022-08-29 03:34:15 -0400

    Created Buildings Table

--- SQL_Tables_Mapper.py
+++ SQL_Tables_Mapper.py
@@ -2,6 +2,7 @@
 
 ### Importing required packages 
 import sqlalchemy 
+from sqlalchemy import Column, Integer, String
 
 ### Setting up the sqlite engine 
 engine = sqlalchemy.create_engine("sqlite://FPS.db", echo = True)
@@ -10,3 +11,9 @@ engine = sqlalchemy.create_engine("sqlite://FPS.db", echo = True)
 base = sqlalchemy.ext.declarative.declarative_base()
 
 ### Declaring the classes for each table 
+class Buildings(base) :
+    __tablename__ = 'Buildings'
+
+    Code = Column(Integer, primary_key = True)
+    Building_Name = Column(String)
+    
