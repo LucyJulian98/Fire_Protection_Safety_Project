62f2f34ec30687ce57b6d303f479aab4c05dddab
LucyJulian98 <llj40@georgetown.edu>
2022-08-29 03:43:44 -0400

    Created database file

--- SQL_Tables_Mapper.py
+++ SQL_Tables_Mapper.py
@@ -3,12 +3,13 @@
 ### Importing required packages 
 import sqlalchemy 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 ### Setting up the sqlite engine 
-engine = sqlalchemy.create_engine("sqlite://FPS.db", echo = True)
+engine = sqlalchemy.create_engine("sqlite:///fps.db", echo = True)
 
 ### Declarative Base Class
-base = sqlalchemy.ext.declarative.declarative_base()
+base = declarative_base()
 
 ### Declaring the classes for each table 
 class Buildings(base) :
@@ -16,4 +17,5 @@ class Buildings(base) :
 
     Code = Column(Integer, primary_key = True)
     Building_Name = Column(String)
-    
+
+base.metadata.create_all(engine)    
