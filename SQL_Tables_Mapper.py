0c779183c8db01479f70eb7d606a4ebcc7951aac
LucyJulian98 <llj40@georgetown.edu>
2022-08-29 10:27:58 -0400

    Added Component, Latest_Inspections, Inspection_Fire_Alarms, Modified Buildings

--- SQL_Tables_Mapper.py
+++ SQL_Tables_Mapper.py
@@ -14,19 +14,13 @@ engine = create_engine("sqlite:///fps.db", echo = True)
 base = declarative_base()
 
 ### Declaring the classes for each table 
-class Buildings(base) :
-    __tablename__ = 'Buildings'
-
-    Code = Column(String, primary_key = True)
-    Building_Name = Column(String)
-    Latitude = Column(String)
-    Longitude = Column(String)
-
-class Devices(base) :
+### Class for both overarching devices and their components
+class Devices(base) :  
     __tablename__ = 'Devices'
 
     Code = Column(String, primary_key = True)
     Device_Name = Column(String)
+
 
 class Systems(base) :
     __tablename__ = 'Systems'
@@ -38,6 +32,21 @@ class Systems(base) :
     Battery_Quantity = Column(Integer)
     Battery_Specs = Column(String)
     Battery_Date = Column(String)
+    Pull_Station_Quantity = Column(Integer)
+    Smoke_Detector_Quantity = Column(Integer)
+    Duct_Detector_Quantity = Column(Integer)
+
+class Buildings(base) :
+    __tablename__ = 'Buildings'
+
+    Code = Column(String, primary_key = True)
+    Building_Name = Column(String)
+    Latitude = Column(String)
+    Longitude = Column(String)
+    System_Id = Column(Integer, ForeignKey("Systems.Id"))
+    System = relationship("Systems", back_populates = "Buildings")
+
+Systems.Building = relationship("Buildings", back_populates = "Systems")
 
 class Pad_Panels(base) :
     __tablename__ = 'Pad_Panels'
@@ -48,7 +57,7 @@ class Pad_Panels(base) :
     Battery_Quantity = Column(Integer)
     Battery_Specs = Column(String)
     Battery_Date = Column(String)
-    system = relationship("Systems", back_populates = "Pad_Panels")
+    System = relationship("Systems", back_populates = "Pad_Panels")
 
 Systems.Pads = relationship("Pad_Panels", back_populates = "Systems")
 
@@ -61,7 +70,7 @@ class Remote_Panels(base) :
     Battery_Quantity = Column(Integer)
     Battery_Specs = Column(String)
     Battery_Date = Column(String)
-    system = relationship("Systems", back_populates = "Remote_Panels") ### First argument class name, second argument table name 
+    System = relationship("Systems", back_populates = "Remote_Panels") ### First argument class name, second argument table name 
 
 Systems.Remotes = relationship("Remote_Panels", back_populates = "Systems")
 
@@ -69,12 +78,51 @@ Systems.Remotes = relationship("Remote_Panels", back_populates = "Systems")
 class Equipment_Ids(base) : 
     __tablename__ = "Equipment_Ids"
 
-    System_Id = Column(Integer, primary_key = True) ### Unable to have foreign key constraint due to devices like hydrants 
+    System_Id = Column(Integer, primary_key = True) ### Unable to have foreign key constraint due to devices like hydrants and kitchen hoods 
     Building_Code = Column(String, primary_key = True) ### Unable to have foreign key constraint due to devices like hydrants 
     Device_Code = Column(String, ForeignKey('Devices.Code'), primary_key = True)    
     device = relationship("Devices", back_populates = "Equipment_Ids") ### First argument class name, second argument table name 
      
 Devices.Equipment_Ids = relationship("Equipment_Ids", back_populates = "Devices")
+
+class Latest_Inspections(base) :
+    __tablename__ = "Latest_Inspections"
+    
+    Inspection_Id = Column(String, primary_key = True) ### From the Inspections Table 
+    Inspection_Type = Column(String)
+    Date = Column(String)
+    System_Issues = Column(Integer)
+
+class Inspections_Fire_Alarms(base) :
+    __tablename__ = "Inspections_Fire_Alarms"
+
+    Id = Column(String, primary_key = True)
+    System_Id = Column(Integer, ForeignKey("Systems.Id"))
+    Date = Column(String)
+    Type = Column(String)
+    Batteries_Tested = Column(Boolean)
+    Pull_Stations_Tested = Column(Integer)
+    Pull_Stations_Failed = Column(Integer)
+    Smoke_Detectors_Tested = Column(Integer)
+    Smoke_Detectors_Failed = Column(Integer)
+    Duct_Detectors_Tested = Column(Integer)
+    Duct_Detectors_Failed = Column(Integer)
+    Source_File_Link = Column(String)
+    System = relationship("Systems", back_populates = "Inspections_Fire_Alarms")
+
+Systems.Fire_Alarm = relationship("Inspections_Fire_Alarms", back_populates = "Systems")
+
+class Components(base) : 
+    __tablename__ = "Components"
+
+    Id = Column(String, primary_key = True)   
+    Equipment_Id = Column(Integer) ### From Equipment ID Table 
+    Component_Code = Column(String, ForeignKey("Devices.Code"))
+    Location = Column(String)
+    Address = Column(String)
+    Devices = relationship("Devices", back_populates = "Components")
+
+Devices.Components = relationship("Components", back_populates = "Devices")
 
 
 
