49111e20885a8c2dc3d661b5608a431b4ac33e39
LucyJulian98 <llj40@georgetown.edu>
2022-09-22 14:48:16 -0400

    Revamped Inspections and added Foam and FM

--- SQL_Tables_Mapper.py
+++ SQL_Tables_Mapper.py
@@ -19,12 +19,6 @@ base = declarative_base()
 ### Declaring the classes for each table 
 
 ################################ SYSTEM INFRASTRUCTURE CLASSES ##################################
-### Class to create unique identifies for each device 
-class Equipment_Ids(base) : 
-    __tablename__ = "Equipment_Ids"
-
-    Id = Column(String, primary_key = True)
-
 
 class Devices(base) :  
     __tablename__ = 'Devices'
@@ -43,6 +37,18 @@ class Components(base) :
     Devices = relationship("Devices", back_populates = "Components")
 
 Devices.Components = relationship("Components", back_populates = "Devices")
+
+### Class to create unique identifies for each device 
+class Equipment_Ids(base) : 
+    __tablename__ = "Equipment_Ids"
+
+    Id = Column(String, primary_key = True)
+    System_Id = Column(Integer) ### Can't place the foreign key constraint due to hydrants and kitchen hoods
+    Building_Code = Column(String) ### Can't place the foreign key constraint due to hydrants 
+    Device_Code = Column(String, ForeignKey("Devices.Code"))
+    Devices = relationship("Devices", back_populates = "Equipment_Ids")
+
+Devices.Equipments_Ids = relationship("Equipment_Ids", back_populates = "Devices")
 
 class Systems(base) :
     __tablename__ = 'Systems'
@@ -122,43 +128,41 @@ Devices.Source_Files = relationship("Source_Files", back_populates = "Devices")
 
 ################################ INSPECTION CLASSES ##################################
 
-### To be populated using triggers or pandas 
-class Latest_Inspections(base) :
-    __tablename__ = "Latest_Inspections"
-    
-    Inspection_Id = Column(String, primary_key = True) ### From the Inspections Table 
-    Inspection_Type = Column(String)
+class Inspections(base) :
+    __tablename__ = "Inspections"
+
+    Id = Column(String, primary_key = True) ### To be automated and generated every time a record is entered 
+    Equipment_Id = Column(String, ForeignKey("Equipment_Ids.Id"))
     Date = Column(String)
-    System_Issues = Column(Integer)
+    Type = Column(String)
+    Source_File_Link_Id = Column(Integer, ForeignKey(Source_Files.Id))
+    Equipment_Ids = relationship("Equipment_Ids", back_populates = "Inspections")
+    Source_Files = relationship("Source_Files", back_populates = "Inspections")
+
+Equipment_Ids.Inspections = relationship("Inspections", back_populates = "Equipment_Ids")
+Source_Files.Inspections = relationship("Inspections", back_populates = "Source_Files")
 
 class Inspections_Fire_Alarms(base) :
     __tablename__ = "Inspections_Fire_Alarms"
 
-    Id = Column(String, primary_key = True)
-    System_Id = Column(Integer, ForeignKey("Systems.Id"))
-    Date = Column(String)
-    Type = Column(String)
+    Id = Column(String, ForeignKey("Inspections.Id"), primary_key = True)
     Batteries_Tested = Column(Boolean)
+    Heat_Detectors_Tested = Column(Integer)
+    Heat_Detectors_Failed = Column(Integer)
     Pull_Stations_Tested = Column(Integer)
     Pull_Stations_Failed = Column(Integer)
     Smoke_Detectors_Tested = Column(Integer)
     Smoke_Detectors_Failed = Column(Integer)
     Duct_Detectors_Tested = Column(Integer)
     Duct_Detectors_Failed = Column(Integer)
-    Source_File_Link_Id = Column(Integer, ForeignKey(Source_Files.Id))
-    Systems = relationship("Systems", back_populates = "Inspections_Fire_Alarms")
-    Source_Files = relationship("Source_Files", back_populates = "Inspections_Fire_Alarms")
+    Inspections = relationship("Inspections", back_populates = "Inspections_Fire_Alarms")
 
-Systems.Inspections_Fire_Alarms = relationship("Inspections_Fire_Alarms", back_populates = "Systems")
-Source_Files.Inspections_Fire_Alarms = relationship("Inspections_Fire_Alarms", back_populates = "Source_Files")
+Inspections.Inspections_Fire_Alarms = relationship("Inspections_Fire_Alarms", back_populates = "Inspections")
 
 class Inspections_Sprinklers(base) :
     __tablename__ = "Inspections_Sprinklers"
 
-    Id = Column(String, primary_key = True)
-    System_Id = Column(Integer, ForeignKey("Systems.Id"))
-    Date = Column(String)
-    Type = Column(String)
+    Id = Column(String, ForeignKey("Inspections.Id"), primary_key = True)
     Dry_System_Problems = Column(Integer)
     Dry_Pipe_Valve_Problems = Column(Integer)
     Air_Compressor_Problems = Column(Integer)
@@ -168,41 +172,58 @@ class Inspections_Sprinklers(base) :
     Supervisory_Devices_Problems = Column(Integer)
     Alarm_Devices_Problems = Column(Integer)
     FDC_Problems = Column(Integer)
-    Source_File_Link_Id = Column(Integer, ForeignKey(Source_Files.Id))
-    Systems = relationship("Systems", back_populates = "Inspections_Sprinklers")
-    Source_Files = relationship("Source_Files", back_populates = "Inspections_Sprinklers")
+    Inspections = relationship("Inspections", back_populates = "Inspections_Sprinklers")
 
-Systems.Inspections_Sprinklers = relationship("Inspections_Sprinklers", back_populates = "Systems")
-Source_Files.Inspections_Sprinklers = relationship("Inspections_Sprinklers", back_populates = "Source_Files")
+Inspections.Inspections_Sprinklers = relationship("Inspections_Sprinklers", back_populates = "Inspections")
 
 class Inspections_Fire_Pumps(base) :
     __tablename__ = "Inspections_Fire_Pumps"
 
-    Id = Column(String, primary_key = True)
-    Equipment_Id = Column(Integer, ForeignKey("Equipment_Ids.Id"))
-    Date = Column(String)
-    Type = Column(String)
+    Id = Column(String, ForeignKey("Inspections.Id"), primary_key = True)
     Fire_Pump_Problems = Column(Integer)
-    Source_File_Link_Id = Column(Integer, ForeignKey(Source_Files.Id))
-    Equipment_Ids = relationship("Equipment_Ids", back_populates = "Inpections_Fire_Pumps")
-    Source_Files = relationship("Source_Files", back_populates = "Inspections_Fire_Pumps")
+    Inspections = relationship("Inspections", back_populates = "Inspections_Fire_Pumps")
 
-Equipment_Ids.Inspections_Fire_Pumps = relationship("Inspections_Fire_Pumps", back_populates = "Equipment_Ids")
-Source_Files.Inspections_Fire_Pumps = relationship("Inspections_Fire_Pumps", back_populates = "Source_Files")
+Inspections.Inspections_Fire_Pumps = relationship("Inspections_Fire_Pumps", back_populates = "Inspections")
 
 class Inspections_Hydrants(base) :
     __tablename__ = "Inspections_Hydrants"
 
-    Id = Column(String, primary_key = True)
-    Hydrant_Id = Column(Integer, ForeignKey("Hydrants.Id"))
-    Date = Column(String)
-    Type = Column(String)
-    Source_File_Link_Id = Column(Integer, ForeignKey(Source_Files.Id))
-    Hydrants = relationship("Hydrants", back_populates = "Inspections_Hydrants")
-    Source_Files = relationship("Source_Files", back_populates = "Inspections_Hydrants")
+    Id = Column(String, ForeignKey("Inspections.Id"), primary_key = True)
+    Hydrant_Testing_Problems = Column(String) ### Ideally yes or no.     
+    Inspections = relationship("Inspections", back_populates = "Inspections_Hydrants")
 
-Hydrants.Inspections_Hydrants = relationship("Inpections_Hydrants", back_populates = "Hydrants")
-Source_Files.Inspections_Hydrants = relationship("Inspections_Hydrants", back_populates = "Source_Files")
+Inspections.Inspections_Hydrants = relationship("Inspections_Hydrants", back_populates = "Inspections")
+
+class Inspections_Foam_Systems(base) :
+    __tablename__ = "Inspections_Foam_Systems"
+
+    Id = Column(String, ForeignKey("Inspections.Id"), primary_key = True)
+    Foam_Testing_Problems = Column(Integer)
+    Inspections = relationship("Inspections", back_populates = "Inspections_Foam_Systems")
+
+Inspections.Inspections_Foam_Systems = relationship("Inspections_Foam_Systems", back_populates = "Inspections")
+
+class Inspections_FM_200(base) :
+    __tablename__ = "Inspections_FM_200"
+
+    Id = Column(String, ForeignKey("Inspections.Id"), primary_key = True)
+    FM_Testing_Problems = Column(Integer)
+    Inspections = relationship("Inspections", back_populates = "Inspections_FM_200")
+
+Inspections.Inspections_FM_200 = relationship("Inspections_FM_200", back_populates = "Inspections")
+
+
+'''
+### To be populated using triggers or pandas 
+class Latest_Inspections(base) :
+    __tablename__ = "Latest_Inspections"
+    
+    Inspection_Id = Column(String, primary_key = True) ### From the Inspections Table 
+    Inspection_Type = Column(String)
+    Date = Column(String)
+    System_Issues = Column(Integer)
+'''
+
 
 ################################ TESTS AND FAILURES ##################################
 
@@ -215,16 +236,18 @@ class Tests(base) :
 
 class Failures(base) :
     __tablename__ = "Failures"
-
+    Inspection_Id = Column(String, ForeignKey("Inspections.Id"), primary_key = True)
     Component_Id = Column(String, ForeignKey("Components.Id"), primary_key = True)
-    Inpsection_Id = Column(String, primary_key = True)
     Test_Id = Column(Integer, ForeignKey("Tests.Id"), primary_key = True)
     Technician_Note = Column(String)
+    Inspections = relationship("Inspections", back_populates = "Failures")
     Components = relationship("Components", back_populates = "Failures")
     Tests = relationship("Tests", back_populates = "Failures")
 
+Inspections.Failures = relationship("Failures", back_populates = "Inspections")
 Components.Failures = relationship("Failures", back_populates = "Components")
 Tests.Failures = relationship("Failures", back_populates = "Tests")
+
 
 ### Create the tables in the database 
 base.metadata.create_all(engine)   
